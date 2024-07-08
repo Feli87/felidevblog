fb69d50f939fdd84ad2fd757df2c785c56d21eb4
FeliDevJs <felipe.traina.azul@gmail.com>
2024-07-08 11:36:08 -0300

    appwriteconfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,72 @@
+"use client";
+import { useState } from "react";
+import { account, ID } from "./appwrite";
 
+const LoginPage = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  const login = async (email: string, password: string) => {
+    const session = await account.createEmailPasswordSession(email, password);
+    //@ts-ignore
+    setLoggedInUser(await account.get());
+  };
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1>Feli Dev Blog</h1>
+  const register = async () => {
+    await account.create(ID.unique(), email, password, name);
+    login(email, password);
+  };
+
+  const logout = async () => {
+    await account.deleteSession("current");
+    setLoggedInUser(null);
+  };
+
+  if (loggedInUser) {
+    return (
+      <div>
+        {/* @ts-ignore */}
+        <p>Logged in as {loggedInUser.name}</p>
+        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>
+          Logout
+        </button>
       </div>
+    );
+  }
 
-
-    </main>
+  return (
+    <div className="flex flex-col w-full h-[100vh] justify-center items-center bg-slate-800 gap-3">
+      <h1 className="text-white text-2xl">FeliDev Blog Log In</h1>
+      <form className="flex flex-col gap-3 w-[300px] text-blue-950">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" onClick={() => login(email, password)}>
+          Login
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" onClick={register}>
+          Register
+        </button>
+      </form>
+    </div>
   );
-}
+};
+
+export default LoginPage;
