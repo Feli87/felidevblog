8b395f81d368f9731dc3c17f47760d9c5418fe0d
FeliDevJs <felipe.traina.azul@gmail.com>
2024-07-09 06:10:41 -0300

    update

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,12 +1,8 @@
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import React from 'react'
 
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const post = await prisma.post.findUnique({
-    where: { id: parseInt(params.id)},
-  });
-
-  return <div>My Post: {post?.title}</div>
+export default function page() {
+  return (
+    <div>Blog</div>
+  )
 }
