eac0b21f5d9d869d84e101afe26e960aa32513bb
FeliDevJs <felipe.traina.azul@gmail.com>
2024-07-09 06:08:10 -0300

    update

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,39 +1,7 @@
+import React from 'react'
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-export default async function Home() {
-  const posts = await prisma.post.findMany();
-
-
-  const addPost = async (formData: FormData)=>{
-    'use server'
-
-    try {
-      await prisma.post.create({
-        data: {
-          title: formData.get('title') as string,
-          published: true,
-          author: { connect: { email: 'felidevjs@gmail.com' } },
-        }
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
+export default function page() {
   return (
-    <div>
-      <h1>Create a New Post</h1>
-      <form action={addPost}>
-        <label>
-          Title
-          <input type="text" name="title" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      {posts?.map((post) => (
-        <p key={post.id}>{post.title}</p>
-      ))}
-    </div>
-  );
+    <div>SignIn</div>
+  )
 }
